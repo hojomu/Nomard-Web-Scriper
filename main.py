19b6325661da7e3ec02a0f4ab9568e82db549f86
hojomu <whdan45@gmail.com>
2023-03-08 16:47:30 +0900

    0308nomard_python

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from requests import get
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
 
-base_url = "https://kr.indeed.com/jobs?q="
-search_term = "python"
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-response = get(f"{base_url}{search_term}")
+browser = webdriver.Chrome(options=options)
 
+browser.get("https://kr.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=1015284880e2ff62")
+
+print(browser.page_source)
+
+'''
 if response.status_code != 200:
     print("Can't request page")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
     job_list = soup.find("ul", class_="jobsearch-Resultslist")
     jobs = job_list.find_all('li', recursive=False)
+    '''
