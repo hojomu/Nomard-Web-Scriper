208405b349629ebe08e884eb2e2234e7e495c604
(hojomu) <(whdan45@gmail.com)>
2023-03-09 23:11:55 +0900

    0309_nomard_python_madeby_home

---
+++ extractors/indeed.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+def get_page_count(keyword):
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    browser = webdriver.Chrome(options=options)
+
+    browser.get(f"https://kr.indeed.com/jobs?q={keyword}")
+
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    pagination = soup.find("nav", role="navigation")
+    if pagination == None:
+        return 1
+    pages = pagination.find_all("div", recursive=False)
+    count = len(pages)
+    if count >= 5:
+        return 5
+    else:
+        return count
+
+def extract_indeed_jobs(keyword):
+    pages = get_page_count(keyword)
+    results = []
+    for page in range(pages):
+        base_url = "https://kr.indeed.com/jobs"
+        final_url = f"{base_url}?q={keyword}&start={page*10}"
+        print(final_url)
+
+        options = Options()
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+        browser = webdriver.Chrome(options=options)
+
+        browser.get(final_url)
+
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        job_list = soup.find("ul", class_="jobsearch-ResultsList")
+        jobs = job_list.find_all("li", recursive=False)
+        for job in jobs:
+
+            zone = job.find("div", class_="mosaic-zone")
+            if zone == None:
+                h2 = job.find("h2", class_="jobTitle")
+                anchor = job.select_one("h2 a")
+                title = anchor['aria-label']
+                link = anchor['href']
+                company = job.find("span", class_="companyName")
+                location = job.find("div", class_="companyLocation")
+                job_data = {
+                    'link' : f"https://kr.indeed.com{link}",
+                    'company' : company.string,
+                    'location' : location.string,
+                    'position' : title
+                }
+                results.append(job_data)
+    return results
